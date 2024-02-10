5bbdc6aa4f2ba34f560183d9bf07598e3188d141
kocmetehan <metehankoc51@gmail.com>
2024-02-10 13:58:38 +0300

    UI is designed

--- ui.py
+++ ui.py
@@ -1,2 +1,28 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+THEME_COLOR = "#375362"
+WHITE = "#FFFFFF"
+
+class QuizInteface():
+    def __init__(self):
+        self.window = Tk()
+
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.canvas = Canvas(width=300, height=250, bg=WHITE)
+        self.canvas.create_text(150,125,text="dfasdfsad", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        #Score 
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        #Buttons
+        right_button_img = PhotoImage(file="./images/false.png")
+        wrong_button_img = PhotoImage(file="./images/true.png")
+        self.right_button = Button(image=right_button_img, highlightbackground=THEME_COLOR)
+        self.wrong_button = Button(image=wrong_button_img, highlightbackground=THEME_COLOR)
+        self.right_button.grid(column=0, row=2)
+        self.wrong_button.grid(column=1, row=2)
+        self.window.mainloop()
